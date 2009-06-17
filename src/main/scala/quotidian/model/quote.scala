26629e63ab6e8bb0b16ee84c48e5845610d377cd
Bryan J Swift <bryan.j.swift@gmail.com>
2009-06-16 22:34:52 -0400

    added id definition and reformatted

--- src/main/scala/quotidian/model/quote.scala
+++ src/main/scala/quotidian/model/quote.scala
@@ -4,7 +4,6 @@ import com.bryanjswift.persistence.{Savable}
 import com.bryanjswift.persistence.annotations.{Entity,Persistent}
 
 @Entity{val name = "Quote"}
-class Quote(
-	@Persistent val text:String,
-	@Persistent val source:String,
-	@Persistent val context:String) extends Savable
+class Quote(@Persistent val text:String, @Persistent val source:String, @Persistent val context:String) extends Savable {
+	val id:Long = 0
+}
