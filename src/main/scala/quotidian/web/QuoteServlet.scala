364e463e18cfdb02bfb57732889d1ee2b93f9c07
Bryan J Swift <bryan.j.swift@gmail.com>
2009-06-28 04:30:46 -0400

    changed Index servlet to QuoteServlet

--- src/main/scala/quotidian/web/QuoteServlet.scala
+++ src/main/scala/quotidian/web/QuoteServlet.scala
@@ -2,7 +2,7 @@ package quotidian.web
 
 import javax.servlet.http.{HttpServlet, HttpServletRequest => Request, HttpServletResponse => Response}
 
-class Index extends HttpServlet {
+class QuoteServlet extends HttpServlet {
 	override def doGet(request:Request, response:Response) {
 		response.sendRedirect("/index.jsp")
 	}
