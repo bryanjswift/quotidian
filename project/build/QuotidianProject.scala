a4a48299dd386044dd09d5483a0478ffb1114ebd
Bryan J Swift <bryan.j.swift@gmail.com>
2009-07-23 20:46:33 -0400

    updated SBT project definition so jars in src/main/lib get included in exploded webapp

--- project/build/QuotidianProject.scala
+++ project/build/QuotidianProject.scala
@@ -2,19 +2,28 @@ import java.io.File
 import sbt._
 
 class QuotidianProject(info:ProjectInfo) extends DefaultWebProject(info) {
+	// locate the Home directory
 	val userHome = system[File]("user.home")
 
+	// Dependencies for compiling
 	val velocity = "org.apache.velocity" % "velocity" % "1.6.1"
 	val commonsCollections = "commons-collections" % "commons-collections" % "3.2.1"
 	val commonsLang = "commons-lang" % "commons-lang" % "2.4"
 
+	// Dependencies for testing
+	val junit = "junit" % "junit" % "4.5" % "test->default"
+	val specs = "org.scala-tools.testing" % "specs" % "1.5.0" % "test->default"
+
+	// App Engine paths
 	val gaeHome = Path.fromFile(userHome.value + "/Documents/src/gae/") / "appengine-java-sdk-1.2.2"
 	val gaeUserJars = gaeHome / "lib" / "user" ** "*.jar"
 	val gaeSharedJars = gaeHome / "lib" / "shared" ** "*.jar"
 
-	val libs = "src" / "main" / "lib" ** "*.jar"
-
-	override def compileClasspath = super.compileClasspath +++ gaeUserJars +++ libs +++ gaeSharedJars 
-
-	//override def testDependencies = Set("org.scala-tools.testing" % "specs" % "1.5.0") ++ super.testDependencies
+	// override looking for jars in ./lib
+	// TODO: need to include gaeUserJars in dependencyPath in order to deploy
+	override def dependencyPath = "src" / "main" / "lib"
+	// override output of war to target/webapp
+	override def temporaryWarPath = outputPath / "war"
+	// compile with App Engine jars
+	override def compileClasspath = super.compileClasspath +++ gaeUserJars +++ gaeSharedJars 
 }
