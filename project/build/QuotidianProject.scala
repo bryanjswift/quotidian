ea1b6f166913d04f797d4bfbafae805faec6e499
Bryan J Swift <bryan.j.swift@gmail.com>
2009-07-25 14:52:15 -0400

    removed jmock dependencies, modified gaeSharedJars to not include jsp jars, added gaeSharedJars to consoleClasspath

--- project/build/QuotidianProject.scala
+++ project/build/QuotidianProject.scala
@@ -13,12 +13,10 @@ class QuotidianProject(info:ProjectInfo) extends DefaultWebProject(info) {
 	// Dependencies for testing
 	val junit = "junit" % "junit" % "4.5" % "test->default"
 	val specs = "org.scala-tools.testing" % "specs" % "1.5.0" % "test->default"
-	val jmock = "org.jmock" % "jmock" % "2.5.1" % "test->default"
-	val jmockLegacy = "org.jmock" % "jmock-legacy" % "2.5.1" % "test->default"
 
 	// App Engine paths
 	val gaeHome = Path.fromFile(userHome.value + "/Documents/src/gae/") / "appengine-java-sdk-1.2.2"
-	val gaeSharedJars = gaeHome / "lib" / "shared" ** "*.jar"
+	val gaeSharedJars = gaeHome / "lib" / "shared" * "*.jar"
 
 	// override looking for jars in ./lib
 	override def dependencyPath = "src" / "main" / "lib"
@@ -26,6 +24,8 @@ class QuotidianProject(info:ProjectInfo) extends DefaultWebProject(info) {
 	override def temporaryWarPath = outputPath / "war"
 	// compile with App Engine jars
 	override def compileClasspath = super.compileClasspath +++ gaeSharedJars 
+	// add App Engine jars to console classpath
+	override def consoleClasspath = super.consoleClasspath +++ gaeSharedJars
 	// compile tests with App Engine jars
 	override def testClasspath = super.testClasspath +++ gaeSharedJars
 	// override path to managed dependency cache
