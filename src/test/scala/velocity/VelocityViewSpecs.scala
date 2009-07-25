0ef9f26e75cf2abf3e0755e07541e80ccb025def
Bryan J Swift <bryan.j.swift@gmail.com>
2009-07-25 15:20:05 -0400

    changed the spec sentences to be more descriptive of what is happenging

--- src/test/scala/velocity/VelocityViewSpecs.scala
+++ src/test/scala/velocity/VelocityViewSpecs.scala
@@ -4,53 +4,51 @@ import java.io.{PrintWriter,StringWriter}
 import org.specs.Specification
 
 object VelocityViewSpecs extends Specification {
-	class Tester {
-		val test = "Tester.test"
-		override def toString() = "Tester.toString"
-	}
 	class Request extends DummyRequest { }
 	class Response extends DummyResponse {
 		val writer = new StringWriter()
 		override def getWriter():PrintWriter = new PrintWriter(writer)
 	}
+	val objTestPath = "templates/objTest.vm"
+	val listTestPath = "templates/listTest.vm"
 
 	def have = addToSusVerb("have")
-	"view.template" should {
-		val view = new VelocityView("templates/listTest.vm")
-		"not be null" in {
+	"Creating a VelocityView with a valid template path" should {
+		val view = new VelocityView(objTestPath)
+		"produce a view with a non-null template" in {
 			view.template must notBeNull
 		}
-	}
-	"view.createVelocityContext" should {
-		val view = new VelocityView("templates/listTest.vm")
-		"return a context with at least one value" in {
+		"produce a non-empty context when passed a valid model" in {
 			val context = view.createVelocityContext(Map("tests" -> List("list test")),null,null)
 			val keys = context.getKeys
 			keys.length mustEqual 1
 		}
 	}
-	"rendering with an object (obj)" should {
-		val view = new VelocityView("templates/objTest.vm")
+	"When rendering a view (" + objTestPath + ") with a simple object (obj), the view" should {
+		val view = new VelocityView(objTestPath)
 		val request = new Request()
 		val response = new Response()
-		val obj = new Tester()
+		val obj = new Object {
+			val test = "Tester.test"
+			override def toString = "Tester.toString"
+		}
 		view.render(Map("obj" -> obj),request,response)
 		val result = response.writer.toString
-		"contain obj.toString" in {
+		"contain the value of obj.toString" in {
 			result indexOf obj.toString must beGreaterThanOrEqualTo(0)
 		}
-		"contain obj.test" in {
+		"contain the value of obj.test" in {
 			result indexOf obj.test must beGreaterThanOrEqualTo(0)
 		}
 	}
-	"rendering with list of Strings" should {
-		val view = new VelocityView("templates/listTest.vm")
+	"When rendering a view (" + listTestPath + ") with a model containing a list, the view" should {
+		val view = new VelocityView(listTestPath)
 		val request = new Request()
 		val response = new Response()
 		val list = List("list test","list test2","another test")
 		view.render(Map("tests" -> list),request,response)
 		val result = response.writer.toString
-		"contain one of the strings" in {
+		"contain the first string" in {
 			result must notEqualIgnoreSpace("")
 			result indexOf list(0) must beGreaterThanOrEqualTo(0)
 		}
