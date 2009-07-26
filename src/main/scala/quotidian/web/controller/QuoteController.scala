1ed536f9ae11ae7b16f45f33cc5c2ca441fc1957
Bryan J Swift <bryan.j.swift@gmail.com>
2009-07-26 02:21:39 -0400

    some reorganization of methods and added get:Quote to QuoteController

--- src/main/scala/quotidian/web/controller/QuoteController.scala
+++ src/main/scala/quotidian/web/controller/QuoteController.scala
@@ -1,25 +1,24 @@
 package quotidian.web.controller
 
 import com.bryanjswift.persistence.Savable
+import java.io.Serializable
 import quotidian.Logging
 import quotidian.model.Quote
 import quotidian.persistence.datastore.DatastorePersister
 
 object QuoteController extends Logging {
-	def all:List[Quote] = {
-		savablesToQuotes(DatastorePersister.getAll(Quote.kind))
-	}
+	def all:List[Quote] = savablesToQuotes(DatastorePersister.getAll(Quote.kind))
+	def bySource(source:String):List[Quote] = savablesToQuotes(DatastorePersister.search(Quote.kind,"source",source))
+	def get(id:Serializable):Quote = DatastorePersister.get(Quote.kind,id).asInstanceOf[Quote]
 	def random:Quote = {
 		val quotes = all
 		val position = (quotes.length * Math.random).toInt
 		quotes(position)
 	}
-	def bySource(source:String):List[Quote] = {
-		savablesToQuotes(DatastorePersister.search(Quote.kind,"source",source))
-	}
-	private def savablesToQuotes(savables:List[Savable]) = {
+	private def savablesToQuotes(savables:List[Savable]):List[Quote] = {
 		for {
 			savable <- savables
 		} yield savable.asInstanceOf[Quote]
 	}
+	def save(quote:Quote):Serializable = DatastorePersister.save(quote)
 }
