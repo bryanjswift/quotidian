249583c411a37f2f72fa5b3ba510af57c6ec4c3a
Bryan J Swift <bryan.j.swift@gmail.com>
2009-08-01 13:54:08 -0400

    abstracted the QuoteController reference in QuoteServlet out to a companion object

--- src/main/scala/quotidian/web/QuoteServlet.scala
+++ src/main/scala/quotidian/web/QuoteServlet.scala
@@ -2,20 +2,24 @@ package quotidian.web
 
 import javax.servlet.http.{HttpServlet, HttpServletRequest => Request, HttpServletResponse => Response}
 import quotidian.model.Quote
-import quotidian.web.controller.QuoteController
+import quotidian.web.controller.{DatastoreQuoteController,QuoteController}
 import velocity.{VelocityHelper,VelocityView}
 
 class QuoteServlet extends HttpServlet {
 	override def doGet(request:Request, response:Response) {
 		val view = new VelocityView("templates/default.vm")
-		view.render(Map("quotes" -> QuoteController.all),request,response)
+		view.render(Map("quotes" -> QuoteServlet.controller.all),request,response)
 	}
 	override def doPost(request:Request, response:Response) {
 		val text = request.getParameterValues("text")(0)
 		val source = request.getParameterValues("source")(0)
 		val context = request.getParameterValues("context")(0)
 		val quote = new Quote(text,source,context)
-		QuoteController.save(quote)
+		QuoteServlet.controller.save(quote)
 		response.sendRedirect("/quote")
 	}
+}
+
+object QuoteServlet {
+	val controller:QuoteController = DatastoreQuoteController
 }
