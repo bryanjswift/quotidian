500825bef450094b6e869fd8f94a1f5c916316f1
Bryan J Swift <bryan.j.swift@gmail.com>
2009-08-09 13:35:47 -0400

    Create additional speces for exercising DatastoreFile

--- src/test/scala/quotidian/search/DatastoreFileSpecs.scala
+++ src/test/scala/quotidian/search/DatastoreFileSpecs.scala
@@ -5,6 +5,7 @@ import quotidian.DatastoreSpecification
 
 object DatastoreFileSpecs extends DatastoreSpecification {
 	"An empty file" should {
+		val byte = 15.toByte
 		"have a position at the start" >> {
 			val file = DatastoreFile()
 			file.position mustEqual 0
@@ -24,18 +25,35 @@ object DatastoreFileSpecs extends DatastoreSpecification {
 			val file = DatastoreFile()
 			file.position mustEqual 0
 			val bits = new Array[Byte](1)
-			file.write(15).read(bits,0,1)
-			bits(0) mustEqual 15
+			file.write(byte).read(bits,0,1)
+			bits(0) mustEqual byte
 		}
 	}
 	"A file with bytes written" should {
-		"provide an entity with the written bytes contained" >> {
-			val ints = (0 until 15)
-			val bytes = for (i <- ints) yield i.asInstanceOf[Byte]
+		val ints = (0 until 15)
+		val bytes = (for (i <- ints) yield i.asInstanceOf[Byte]).toArray
+		"be able to read all written bytes" >> {
 			val file = DatastoreFile()
 			val bits = new Array[Byte](15)
-			file.write(bytes.toArray,0,15).read(bits,0,15)
-			bits must haveTheSameElementsAs(bytes)
+			file.write(bytes,0,15).read(bits,0,15)
+			bits must containInOrder(bytes)
+		}
+		"provide an entity with the written bytes contained" >> {
+			val file = DatastoreFile().write(bytes,0,15)
+			val bits = file.entity.getProperty(DatastoreFile.Contents).asInstanceOf[Blob].getBytes
+			bits must containInOrder(bytes)
+		}
+		"have a length equal to the number of bytes written" >> {
+			val file = DatastoreFile().write(bytes,0,15)
+			file.length mustEqual bytes.length
+		}
+		"be able to seek within the bytes written" >> {
+			val file = DatastoreFile().write(bytes,0,15)
+			file.seek(bytes.length - 5).position mustEqual(bytes.length - 5)
+		}
+		"be able to seek outside the bytes written" >> {
+			val file = DatastoreFile().write(bytes,0,15)
+			file.seek(bytes.length + 5).position mustEqual(bytes.length + 5)
 		}
 	}
 }
