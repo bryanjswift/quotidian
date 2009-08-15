58201d73cd0c36650506be7cae8822b3b728faae
Bryan J Swift <bryan.j.swift@gmail.com>
2009-08-15 11:37:55 -0400

    Add class and method to easily convert Entity to Quote

--- src/main/scala/quotidian/persistence/datastore/PersisterHelper.scala
+++ src/main/scala/quotidian/persistence/datastore/PersisterHelper.scala
@@ -3,6 +3,7 @@ package quotidian.persistence.datastore
 import basic.persistence.Savable
 import com.google.appengine.api.datastore.Entity
 import quotidian.Logging
+import quotidian.model.Quote
 import scala.collection.jcl.Conversions._
 import scala.xml.{NodeSeq,XML}
 
@@ -11,9 +12,9 @@ object PersisterHelper extends Logging {
 	def toXml(entity:Entity):NodeSeq = {
 		val properties = entity.getProperties	
 		val keys = properties.keySet
-		val nodes = for {
+		val nodes = List("<id>" + entity.getKey.getId + "</id>") ++ (for {
 			val key <- keys
-		} yield "<" + key + ">" + properties.get(key) + "</" + key + ">"
+		} yield "<" + key + ">" + properties.get(key) + "</" + key + ">")
 		val s = nodes.mkString("<" + entity.getKind() + ">","","</" + entity.getKind() + ">")
 		XML.loadString(s)
 	}
@@ -26,4 +27,14 @@ object PersisterHelper extends Logging {
 			case None => null
 		}
 	}
+	def entity2quote(entity:Entity):Quote = {
+		val e = new RichEntity(entity)
+		Quote(e.id,e.get[String]("text"),e.get[String]("source"),e.get[String]("context"))
+	}
 }
+
+class RichEntity(private val entity:Entity) {
+	def get[T <: AnyRef](property:String):T = entity.getProperty(property).asInstanceOf[T]
+	def id = entity.getKey.getId
+}
+
