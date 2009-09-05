2edccff407619ff3dec5dcd89134d7df99031cfe
Bryan J Swift <bryan.j.swift@gmail.com>
2009-09-04 23:11:04 -0400

    Create more representative Quote object for specs

--- src/test/scala/quotidian/web/controller/DatastoreQuoteControllerSpecs.scala
+++ src/test/scala/quotidian/web/controller/DatastoreQuoteControllerSpecs.scala
@@ -8,10 +8,14 @@ object DatastoreQuoteControllerSpecs extends DatastoreSpecification {
 	val controller = new DatastoreQuoteController
 	"A controller" should {
 		datastoreCleanup.before
-		val quote = Quote("text","source","context")
+		val quote = Quote("this is some text","this is a source","and this is context")
 		"save a quote" >> {
 			val key = controller.save(quote)
 			key must notBeNull
+		}
+		"create files when saving a quote" >> {
+			controller.save(quote)
+			controller.directory.list.length must beGreaterThan(0)
 		}
 		"have a searcher with a reader with terms" >> {
 			controller.save(quote)
