80d1b595d933bc3bfc5136676ec0676d84a15dcc
Bryan J Swift <bryan.j.swift@gmail.com>
2009-09-06 21:24:00 -0400

    added spec searching for words unique to source

--- src/test/scala/quotidian/web/controller/DatastoreQuoteControllerSpecs.scala
+++ src/test/scala/quotidian/web/controller/DatastoreQuoteControllerSpecs.scala
@@ -1,9 +1,9 @@
 package quotidian.web.controller
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer
-import org.apache.lucene.index.{IndexReader,IndexWriter}
+import org.apache.lucene.index.{IndexReader,IndexWriter,Term}
 import org.apache.lucene.index.IndexWriter.MaxFieldLength.UNLIMITED
-import org.apache.lucene.search.IndexSearcher
+import org.apache.lucene.search.{FuzzyQuery,IndexSearcher,TermQuery}
 import quotidian.DatastoreSpecification
 import quotidian.model.Quote
 import quotidian.search.DatastoreDirectory
@@ -23,6 +23,20 @@ object DatastoreQuoteControllerSpecs extends DatastoreSpecification {
 			val terms = reader.terms
 			terms.next mustEqual true
 			reader.close
+		}
+		"be able to find Quotes written to it" >> {
+			val directory = new DatastoreDirectory()
+			val writer = new IndexWriter(directory,new StandardAnalyzer(),UNLIMITED)
+			writer.addDocument(quote)
+			writer.commit
+			val searcher = new IndexSearcher(directory)
+			val sourceTerm = new Term(Quote.Source)
+			val scoreDocs = searcher.search(new FuzzyQuery(sourceTerm.createTerm("this source")),10).scoreDocs
+			val quotes = for {
+				scoreDoc <- scoreDocs
+				doc = searcher.doc(scoreDoc.doc)
+			} yield Quote(doc.getValues(Quote.Text)(0),doc.getValues(Quote.Source)(0),doc.getValues(Quote.Context)(0))
+			quotes.length must beGreaterThan(0)
 		}
 	}
 	"A controller" should {
