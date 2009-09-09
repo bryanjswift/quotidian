43ee1ddef16aeb06866580b16b435a3c59ca79fe
Bryan J Swift <bryan.j.swift@gmail.com>
2009-09-08 22:27:38 -0400

    Add some more test cases

--- src/test/scala/quotidian/web/controller/DatastoreQuoteControllerSpecs.scala
+++ src/test/scala/quotidian/web/controller/DatastoreQuoteControllerSpecs.scala
@@ -11,13 +11,15 @@ import quotidian.search.DatastoreDirectory
 object DatastoreControllerSpecs extends DatastoreSpecification {
 	val quoteController = new DatastoreQuoteController
 	val searchController = new DatastoreSearchController
-	val quote = Quote("I like oysters but they never make me think 'I want dick in me'.","Mary Lee","A barbeque after some drinks")
+	val q1 = Quote("I like oysters but they never make me think 'I want dick in me'.","Mary Lee","A barbeque after some drinks")
+	val q2 = Quote("You should eat more vegetables","Jeremy Swift","In regards to my dark circles")
+	val q3 = Quote("But I need you desperately","","Entourage theme song")
 	"A DatastoreDirectory" should {
 		datastoreCleanup.after
 		"be able to have Quotes written to it" >> {
 			val directory = new DatastoreDirectory
 			val writer = new IndexWriter(directory,new StandardAnalyzer(),UNLIMITED)
-			writer.addDocument(quote)
+			writer.addDocument(q1)
 			writer.commit
 			val searcher = new IndexSearcher(directory)
 			val reader = searcher.getIndexReader
@@ -28,7 +30,7 @@ object DatastoreControllerSpecs extends DatastoreSpecification {
 		"be able to find Quotes written to it" >> {
 			val directory = new DatastoreDirectory()
 			val writer = new IndexWriter(directory,new StandardAnalyzer(),UNLIMITED)
-			writer.addDocument(quote)
+			writer.addDocument(q1)
 			writer.commit
 			val searcher = new IndexSearcher(directory)
 			val scoreDocs = searcher.search(new TermQuery(new Term("source","Mary")),10).scoreDocs
@@ -42,26 +44,26 @@ object DatastoreControllerSpecs extends DatastoreSpecification {
 	"A controller" should {
 		datastoreCleanup.after
 		"save a quote" >> {
-			val key = quoteController.save(quote)
+			val key = quoteController.save(q1)
 			key must notBeNull
 		}
 		"have a searcher with a reader with terms" >> {
-			quoteController.save(quote)
+			quoteController.save(q1)
 			val terms = searchController.searcher.getIndexReader.terms
 			terms.next mustEqual true
 		}
 		"find Quotes by source" >> {
-			val key = quoteController.save(quote)
+			val key = quoteController.save(q1)
 			val quotes = searchController.searchSource("Mary")
 			quotes.length must beGreaterThan(0)
 		}
 		"find Quotes by context" >> {
-			val key = quoteController.save(quote)
+			val key = quoteController.save(q1)
 			val quotes = searchController.searchContext("barbeque")
 			quotes.length must beGreaterThan(0)
 		}
 		"find Quotes by text" >> {
-			val key = quoteController.save(quote)
+			val key = quoteController.save(q1)
 			val quotes = searchController.searchText("dick")
 			quotes.length must beGreaterThan(0)
 		}
