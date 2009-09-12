345bf62ec86e2e694c7b07b7037d3a48a381414c
Bryan J Swift <bryan.j.swift@gmail.com>
2009-09-12 18:33:19 -0400

    Moved GaeEnvironment into DatastoreSpecification.scala
    Made GaeEnvironment an inner class of the DatastoreService object

--- src/test/scala/quotidian/DatastoreSpecification.scala
+++ src/test/scala/quotidian/DatastoreSpecification.scala
@@ -3,6 +3,7 @@ package quotidian
 import com.google.appengine.api.datastore.dev.LocalDatastoreService
 import com.google.appengine.tools.development.ApiProxyLocalImpl
 import com.google.apphosting.api.ApiProxy
+import com.google.apphosting.api.ApiProxy.Environment
 import java.io.File
 import org.specs.Specification
 
@@ -11,8 +12,20 @@ class DatastoreSpecification extends Specification {
 	val proxy = new ApiProxyLocalImpl(new File("./target/")){ }
 	proxy.setProperty(LocalDatastoreService.NO_STORAGE_PROPERTY,true.toString)
 	val service = proxy.getService("datastore_v3").asInstanceOf[LocalDatastoreService]
+	private class GaeEnvironment(private val requestNamespace:String) extends Environment {
+		def this() = this("")
+		def getAppId = "Quotidian Specs"
+		def getVersionId = "1.0"
+		def getRequestNamespace = requestNamespace
+		def getAuthDomain = "gmail.com"
+		def isLoggedIn = throw new UnsupportedOperationException()
+		def getEmail = ""
+		def isAdmin = throw new UnsupportedOperationException()
+		def getAttributes = new java.util.HashMap[String,Object]()
+	}
 	def datastoreSetup:Unit = {
-		ApiProxy.setEnvironmentForCurrentThread(new GaeEnvironment)
+		val env = new GaeEnvironment
+		ApiProxy.setEnvironmentForCurrentThread(env)
 		ApiProxy.setDelegate(proxy)
 	}
 	def datastoreTeardown:Unit = {
@@ -27,3 +40,4 @@ class DatastoreSpecification extends Specification {
 	doBeforeSpec { datastoreSetup }
 	doAfterSpec { datastoreTeardown }
 }
+
