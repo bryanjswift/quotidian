626355586155a94a3dcce043c9d1d2e27919338e
Bryan J Swift <bryan.j.swift@gmail.com>
2009-09-19 14:09:59 -0400

    Add implementation for ExpandingTextarea class

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -20,10 +20,29 @@
 	});
 	var ExpandingTextarea = new Class({
 		initialize: function(el) {
-			this.el = document.id(el);
+			this.el = el.el ? el.el : $(el);
+			this.height = this.el.getSize().y;
+			this.clone = (this.el.clone()
+					.setStyles({position:'absolute',width:this.el.getStyle('width'),visibility:'hidden',overflow:'hidden'})
+					.set({id:this.el.get('id') + 'clone',value:'',disabled:'disabled'}));
+			this.el.getParent().adopt(this.clone);
+			var update = this.keypress.bind(this);
+			this.el.addEvents({keydown:update,keypress:update,keyup:update}).addClass('expandingTextarea').set('tween',{duration:250});
+		},
+		keypress: function(e) {
+			var evt = new Event(e);
+			this.clone.set('value',this.el.get('value'));
+			this.update();
+		},
+		update: function() {
+			var currentHeight = this.clone.getScrollSize().y;
+			if (currentHeight != this.height) {
+				this.height = currentHeight;
+				this.el.tween('height',currentHeight);
+			}
 		}
 	});
-	var text = new TextAsLabelInput('text');
+	var text = new TextAsLabelInput(new ExpandingTextarea('text'));
 	var source = new TextAsLabelInput('source');
 	var context = new TextAsLabelInput('context');
 	function redrawElement() {
