c6bacb24fd26f13bc3faeb3f18165a4659b7660c
Bryan J Swift <bryan.j.swift@gmail.com>
2009-09-19 21:29:41 -0400

    Add page and delete methods and update QuoteServlet to use them

--- src/main/scala/quotidian/web/QuoteServlet.scala
+++ src/main/scala/quotidian/web/QuoteServlet.scala
@@ -9,7 +9,7 @@ import velocity.{VelocityHelper,VelocityView}
 class QuoteServlet extends HttpServlet {
 	override def doGet(request:Request, response:Response) {
 		val view = new VelocityView("templates/default.vm")
-		view.render(Map("quotes" -> QuoteServlet.qc.all),request,response)
+		view.render(Map("quotes" -> QuoteServlet.qc.page(1)),request,response)
 	}
 	override def doPost(request:Request, response:Response) {
 		val text = request.getParameterValues(Quote.Text)(0)
@@ -17,7 +17,7 @@ class QuoteServlet extends HttpServlet {
 		val context = request.getParameterValues(Quote.Context)(0)
 		val quote = new Quote(text,source,context)
 		QuoteServlet.qc.save(quote)
-		response.sendRedirect("/quote")
+		response.sendRedirect("/")
 	}
 }
 
--- src/main/scala/quotidian/web/controller/QuoteController.scala
+++ src/main/scala/quotidian/web/controller/QuoteController.scala
@@ -14,7 +14,8 @@ abstract class QuoteController extends Logging {
 	protected def directory:Directory
 	protected def MaxPerPage:Int
 	protected def writer:IndexWriter = new IndexWriter(directory,new StandardAnalyzer(),UNLIMITED)
-	def all:List[Quote] = persister.some(Quote.Kind,MaxPerPage,0)
+	def all:List[Quote] = persister.all(Quote.Kind)
+	def delete(id:Serializable):Unit = persister.delete(Quote.Kind,id)
 	def get(id:Serializable):Quote = persister.get(Quote.Kind,id).asInstanceOf[Quote]
 	def random:Quote = {
 		val quotes = all
@@ -32,9 +33,11 @@ abstract class QuoteController extends Logging {
 		}
 		key
 	}
-	implicit private def savablesToQuotes(savables:List[Savable]):List[Quote] = {
+	def page(pageNumber:Int):List[Quote] = persister.some(Quote.Kind,MaxPerPage,0)
+	implicit private def savables2quotes(savables:List[Savable]):List[Quote] = {
 		for {
 			savable <- savables
 		} yield savable.asInstanceOf[Quote]
 	}
+	implicit private def savable2quote(savable:Savable):Quote = savable.asInstanceOf[Quote]
 }
