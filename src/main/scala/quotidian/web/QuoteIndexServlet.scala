44b2e2520444a83141447343bd74f1e403065081
Bryan J Swift <bryan.j.swift@gmail.com>
2010-02-26 17:05:47 -0500

    Call QuoteController.get in order to get the Quote
    Do this rather than constructing it because constructing it would mean
    creating a valid Key object in the 'ui aware' code. I'm trying to keep
    the app engine abstracted out as much as possible. Also added some
    logging.

--- src/main/scala/quotidian/web/QuoteIndexServlet.scala
+++ src/main/scala/quotidian/web/QuoteIndexServlet.scala
@@ -9,16 +9,17 @@ class QuoteIndexServlet extends HttpServlet with Logging {
 	override def doPost(request:Request, response:Response) {
 		val http = HttpHelper(request,response)
 		val id = http(Quote.Id)
-		val text = http(Quote.Text,Labels(Quote.Text))
-		val source = http(Quote.Source,Labels(Quote.Source))
-		val context = http(Quote.Context,Labels(Quote.Context))
-		val quote = Quote(id.get,text.get,source.getOrElse(""),context.getOrElse(""))
+		val quote = Config.qc.get(id.get)
 		info("Processing index task for " + quote.toString)
 		try {
 			Config.qc.index(quote)
+			info("Indexed " + quote.toString + " successfully")
 			response.setStatus(Response.SC_OK)
 		} catch {
-			case e => response.setStatus(Response.SC_INTERNAL_SERVER_ERROR)
+			case e => {
+				warn("Failed to index " + quote.toString)
+				response.setStatus(Response.SC_INTERNAL_SERVER_ERROR)
+			}
 		}
 	}
 }
