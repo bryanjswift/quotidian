7ed0f3d4f57a49cd82122cc17d828b40a8c9ba45
Bryan J Swift <bryan.j.swift@gmail.com>
2010-02-26 14:26:09 -0500

    Add HttpHelper to get params ignoring default values
    Modify QuoteServlet to make use of HttpHelper

--- src/main/scala/quotidian/web/QuoteServlet.scala
+++ src/main/scala/quotidian/web/QuoteServlet.scala
@@ -7,30 +7,27 @@ import quotidian.web.controller.{QuoteController,SearchController}
 import velocity.{VelocityHelper,VelocityView}
 
 class QuoteServlet extends HttpServlet {
-	val labels = Map(
+	val Labels = Map(
 		Quote.Text -> "*Words to remember...",
 		Quote.Source -> "The wordsmith..",
 		Quote.Context -> "Provide some context..")
 	override def doGet(request:Request, response:Response) = doGet(request,response,Nil)
 	def doGet(request:Request,response:Response,errors:List[String]) = {
 		val view = new VelocityView("templates/default.vm")
-		view.render(Map("quotes" -> QuoteServlet.qc.page(1),"errors" -> errors) ++ labels,request,response)
+		view.render(Map("quotes" -> QuoteServlet.qc.page(1),"errors" -> errors) ++ Labels,request,response)
 	}
 	override def doPost(request:Request, response:Response) {
-		def getParameter(param:String,default:String) = {
-			val value = request.getParameter(param)
-			if (value == "" || value == default) None else Some(value)
-		}
-		val text = getParameter(Quote.Text,labels(Quote.Text))
-		val source = getParameter(Quote.Source,labels(Quote.Source))
-		val context = getParameter(Quote.Context,labels(Quote.Context))
+		val http = HttpHelper(request,response)
+		val text = http(Quote.Text,Labels(Quote.Text))
+		val source = http(Quote.Source,Labels(Quote.Source))
+		val context = http(Quote.Context,Labels(Quote.Context))
 		var errors:List[String] = Nil
 		try {
 			val quote = new Quote(text.get,source.getOrElse(""),context.getOrElse(""))
 			QuoteServlet.qc.save(quote)
 		} catch {
 			case nsee:NoSuchElementException =>
-				errors = "\"" + labels(Quote.Text) + "\" is required." :: errors
+				errors = "\"" + Labels(Quote.Text) + "\" is required." :: errors
 		}
 		if (errors.length > 0) { doGet(request,response,errors) }
 		else { response.sendRedirect("/") }
