f7c0c53ff20dc0f724eccde459d59c3a8c8b31a1
Bryan J Swift <bryan.j.swift@gmail.com>
2010-02-26 22:05:56 -0500

    Add search label to Config.Labels

--- src/main/scala/quotidian/Config.scala
+++ src/main/scala/quotidian/Config.scala
@@ -1,7 +1,7 @@
 package quotidian
 
 import java.util.Properties
-import quotidian.model.Quote
+import quotidian.model.{Quote,Search}
 import quotidian.web.controller.{QuoteController,SearchController}
 
 object Config {
@@ -10,7 +10,8 @@ object Config {
 	val Labels = Map(
 		Quote.Text -> "*Words to remember...",
 		Quote.Source -> "The wordsmith..",
-		Quote.Context -> "Provide some context..")
+		Quote.Context -> "Provide some context..",
+		Search.Key -> "Find some words..")
 	val IndexQuoteQueue = "index-quote"
 	def apply(property:String) = properties.getProperty(property)
 	def objectForProperty[T](property:String) = Class.forName(apply(property)).getConstructor().newInstance().asInstanceOf[T]
